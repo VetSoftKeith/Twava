54051366a8d332f219ab24bc92142abdd2835990
Ague Mort <aguemort@outlook.com>
2018-02-04 15:39:37 -0700

    rename CreateClipResource to just CreateClip

---
+++ src/main/java/us/vetsoft/twitch/helix/resources/CreateClip.java
@@ -0,0 +1,7 @@
+package us.vetsoft.twitch.helix.resources;
+
+public class CreateClip {
+    public CreateClip(String broadcasterId, String clientId, String userToken) {
+
+    }
+}
--- src/main/java/us/vetsoft/twitch/helix/resources/CreateClipResource.java
+++
@@ -1,7 +0,0 @@
-package us.vetsoft.twitch.helix.resources;
-
-public class CreateClipResource {
-    public CreateClipResource(String broadcasterId, String clientId, String userToken) {
-
-    }
-}
